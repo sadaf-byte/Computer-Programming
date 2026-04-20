a21adba3af94ca4a6fa963ae575b326fa041c3c6
Sadaf shaheen <s99288137@gmail.com>
2026-04-20 20:48:29 +0500

    Add files via upload

---
+++ Lab 3 /lab 3 work/Lab 3 Labwork 3.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;  // variable to store number
+
+    cout << "Enter a number: ";
+    cin >> num;  // read number from user
+
+    if (num > 0)  // check if number is positive
+        cout << "The number is positive." << endl;
+    else
+        cout << "The number is not positive." << endl;
+
+    return 0;  // program ends
+}
---
+++ Lab 3 /lab 3 work/Lab 3 Labwork 4.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;  // variable to store age
+
+    cout << "Enter your age: ";
+    cin >> age;  // read age from user
+
+    if (age >= 18)  // check if age is 18 or older
+        cout << "You are 18 or older." << endl;
+    else
+        cout << "You are under 18." << endl;
+
+    return 0;  // program ends
+}
