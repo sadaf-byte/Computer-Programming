d23cd66ebe941ea61c34f8b55d64a24ae4fa0cee
Sadaf shaheen <s99288137@gmail.com>
2026-04-20 20:34:50 +0500

    Add files via upload

---
+++ Lab 2/lab 2 work/Lab 2 Labwork 2.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float pi = 3.14;  // declare float variable pi
+    cout << "Value of pi = " << pi << endl;  // display the value
+    return 0;
+}
+
+
---
+++ Lab 2/lab 2 work/Lab 2 labwork 3.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float celsius, fahrenheit;  // declare variables
+
+    cout << "Enter temperature in Celsius: ";
+    cin >> celsius;  // take input from user
+
+    // Conversion formula
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    cout << "Temperature in Fahrenheit: " << fahrenheit << endl;
+
+    return 0;
+}
+
