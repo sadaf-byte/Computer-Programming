ee957426137b6255b0708d915202e8fbf7086e8f
Sadaf shaheen <s99288137@gmail.com>
2026-04-20 19:36:07 +0500

    Add files via upload

---
+++ Lab 1/lab 1 work/lab 1 Lab work 1.cpp
@@ -0,0 +1,18 @@
+
+// Name: Sadaf shaheen
+// Department: Electrical Engineering (AI)
+// Registration No: BF25NWELE0693
+// Section: B
+
+#include <iostream>   // Header file for input and output
+using namespace std;  // Using standard namespace
+
+int main() 
+{
+    // Printing Hello World on the screen
+    cout << "Hello World!";
+
+    return 0;  // Program ends here
+}
+
+
